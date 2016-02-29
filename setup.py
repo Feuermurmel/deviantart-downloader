c974a8a101b59655216f5b22f56dcdc72a415a74
Michael Schwarz <michi.schwarz@gmail.com>
2016-02-29 23:49:28 +0100

    Added project name and version.

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import setuptools
 
 
 setuptools.setup(
+	name='ponydl',
+	version='0.1',
 	packages = setuptools.find_packages(),
 	install_requires = ['beautifulsoup4', 'requests', 'unidecode'],
 	entry_points = dict(
