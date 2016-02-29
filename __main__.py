5654325c3e3db8a9416f544615a57dee79052351
Michael Schwarz <michi.schwarz@gmail.com>
2016-02-29 23:35:51 +0100

    Use argparse to parse command line.

--- __main__.py
+++ __main__.py
@@ -1,9 +1,17 @@
-import sys
+import argparse
 from lib import deviantart
+
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+	
+	parser.add_argument('user', help = 'Username of the account from which to download ALL THE IMAGES!')
+	
+	return parser.parse_args()
 
 
 def main(user):
 	deviantart.download_user_images(user)
 
 
-main(*sys.argv[1:])
+main(**vars(parse_args()))
